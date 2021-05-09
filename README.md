d5cb160ed73f8835843e4e8b98856f615a0bce0c
Shubham Gawle <shubham.gawle@ril.com>
2021-05-09 13:37:00 +0530

    Added README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+#Assessment test by Randstad
+
+1. This project is the solution of assessment test by Randstad.
+2. Project is deployed on Heroku:
+    [Heroku](https://randstad-task.herokuapp.com/)
+    1. Problem statement:
+        [Problem_Statement](https://randstad-task.herokuapp.com/download/problem)
+    1. Task 1 solution:
+        [Task1](https://randstad-task.herokuapp.com/download/task1)
+    3. Task 2 solution:
+        [Task2](https://randstad-task.herokuapp.com/download/task2)
+3. To run project on local:
+    1. git clone project
+    2. python3 and pip should be installed. if not install them.
+    3. Go to root directory, make and activate virtual env.
+    4. run the command `pip install -r requirements.txt`
+    5. run the app.py file
+    6. go to http://localhost:5000/
+        1. task1 is http://localhost:5000/download/task1
+        2. task2 is http://localhost:5000/download/task2
+        3. problem is http://localhost:5000/download/problem
+    
+
+   
+   
+
--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,9 @@ import os
 
 @app.route('/',methods=['GET', 'POST'])
 def home():
-    return "Hello world!!!!"
+    return "<h1>Assessment test by Randstad</h1><h3>By- Shubham Gawle</h3> <h4>shubham.gawle.ece14@itbhu.ac.in</h4>"
 
-@app.route('/randstad_task/download/task1', methods=['GET'])
+@app.route('/download/task1', methods=['GET'])
 def download_task1():
     try:
         url1 = app.config['WHO_URL']
@@ -21,12 +21,22 @@ def download_task1():
     except Exception as e:
         raise e
 
-@app.route('/randstad_task/download/task2', methods=['GET'])
+@app.route('/download/task2', methods=['GET'])
 def download_task2():
     try:
         parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         file_path = os.path.join(parent_dir,'solutions')
         print("file_path",file_path)
         return send_file(file_path, as_attachment=True,attachment_filename='randstad_task2')
+    except Exception as e:
+        raise e
+
+@app.route('/download/problem', methods=['GET'])
+def problem():
+    try:
+        parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        file_path = os.path.join(parent_dir,'PythonTask.docx')
+        print("file_path",file_path)
+        return send_file(file_path, as_attachment=True,attachment_filename='PythonTask.docx')
     except Exception as e:
         raise e
